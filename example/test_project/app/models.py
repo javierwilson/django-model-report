db308e6f54aa3b6f4b329ac53dcddf3faabf5fe9
Javier Wilson <javier.wilson@gmail.com>
2015-10-11 09:17:51 -0600

    null has no effect on ManyToManyField.

--- example/test_project/app/models.py
+++ example/test_project/app/models.py
@@ -84,8 +84,8 @@ class Support(models.Model):
 class Browser(models.Model):
     name = models.CharField(_('Name'), max_length=25)
     is_active = models.BooleanField(_('Is Active?'), default=True)
-    run_on = models.ManyToManyField(OS, verbose_name=_('Run On'), related_name='browsers', blank=True, null=True)
-    supports = models.ManyToManyField(Support, verbose_name=_('Supports'), related_name='browsers', blank=True, null=True)
+    run_on = models.ManyToManyField(OS, verbose_name=_('Run On'), related_name='browsers', blank=True)
+    supports = models.ManyToManyField(Support, verbose_name=_('Supports'), related_name='browsers', blank=True)
 
     class Meta:
         verbose_name = _('Browser')
