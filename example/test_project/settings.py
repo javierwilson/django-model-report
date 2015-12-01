aea4ba32e8c645f12f89bddf973cbb0c8b8b1bed
Javier Wilson <javier.wilson@gmail.com>
2015-12-01 10:14:09 -0600

    rename database (and gitignore it)

--- example/test_project/settings.py
+++ example/test_project/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'model_report',                      # Or path to database file if using sqlite3.
+        'NAME': 'model_report.db',                      # Or path to database file if using sqlite3.
         # 'USER': '',                      # Not used with sqlite3.
         # 'PASSWORD': '',                  # Not used with sqlite3.
         # 'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
